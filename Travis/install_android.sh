a0913b85bc73579c88fd772773eaaf08d5dba9d7
YdeaGames <ydeagames@gmail.com>
2019-04-13 04:48:04 +0900

    android env

--- Travis/install_android.sh
+++ Travis/install_android.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 
-brew install caskroom/cask/android-sdk caskroom/cask/android-ndk
-export ANDROID_SDK_ROOT=/usr/local/share/android-sdk
-export ANDROID_NDK_ROOT=/usr/local/share/android-ndk
+# brew cask install caskroom/cask/android-sdk
+# brew cask install caskroom/cask/android-ndk
+# export ANDROID_SDK_ROOT="/usr/local/share/android-sdk"
+# export ANDROID_NDK_ROOT="/usr/local/share/android-ndk"
 # sdkmanager --package_file=[sdkmanagerのドキュメントを参照して依存を書いたファイルを指定して下さい]
