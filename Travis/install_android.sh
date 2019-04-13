6e4d289a1a5517a7ee2597f6e9b51c819338b02b
YdeaGames <ydeagames@gmail.com>
2019-04-13 18:50:05 +0900

    Revert "bash"
    This reverts commit 68aa35b3d8b40b6c332865acb1cf0a32b9336f4b.

--- Travis/install_android.sh
+++ Travis/install_android.sh
@@ -1,4 +1,4 @@
-#! /bin/bash
+#! /bin/sh
 
 echo "Attempting to Install Android SDK and NDK"
 brew tap homebrew/cask
@@ -6,5 +6,5 @@ mkdir -p ~/.android/
 touch ~/.android/repositories.cfg # without this file, error will occur on next step
 brew cask install android-sdk
 brew cask install android-ndk
-yes | sdkmanager --licenses > /dev/null
-sdkmanager "platform-tools" "platforms;android-28" "build-tools;28.0.3" > /dev/null
+yes | $ANDROID_SDK_ROOT/tools/sdkmanager --licenses > /dev/null
+$ANDROID_SDK_ROOT/tools/sdkmanager "platform-tools" "platforms;android-28" "build-tools;28.0.3" > /dev/null
