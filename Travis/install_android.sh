0f900d07906d15075aacf5e90c31c75664b38691
YdeaGames <ydeagames@gmail.com>
2019-04-13 17:21:58 +0900

    remove env

---
+++ Travis/install_android.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+
+echo "Attempting to Install Android SDK and NDK"
+brew tap homebrew/cask
+brew cask uninstall java # uninstall java9
+brew tap homebrew/cask-versions
+brew cask install java8 # install java8
+mkdir -p ~/.android/
+touch ~/.android/repositories.cfg # without this file, error will occur on next step
+brew cask install android-sdk
+brew cask install android-ndk
+yes | sdkmanager --licenses > /dev/null
+sdkmanager "platform-tools" "platforms;android-28" "build-tools;28.0.3" > /dev/null
