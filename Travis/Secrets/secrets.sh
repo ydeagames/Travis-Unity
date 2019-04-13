abbd050fb6ff5d7a0fb7bb460c37207fb36feb3f
YdeaGames <ydeagames@gmail.com>
2019-04-14 06:22:20 +0900

    ipa

---
+++ Travis/Secrets/secrets.sh
@@ -0,0 +1,6 @@
+echo 'Generating Secrets...'
+tar cvf secrets.tar travis.p12 travis.mobileprovision
+cd ../../
+echo y | travis encrypt-file ./Travis/Secrets/secrets.tar ./Travis/Secrets/secrets.tar.enc
+echo 'Generating Secrets Completed'
+sleep 16
