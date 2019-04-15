3cf893fdc7a438b42b54c531ed4f8db58da2b2b3
YdeaGames <ydeagames@gmail.com>
2019-04-15 20:01:13 +0900

    settings

--- Travis/deploygate.sh
+++ Travis/deploygate.sh
@@ -1,5 +1,8 @@
 #! /bin/sh
 
-if [ "${TRAVIS_BRANCH}" = "stable" -a "${TRAVIS_PULL_REQUEST}" = "false" ]; then
-  curl -F "file=@./Build/android.apk" -F "token=${DEPLOYGATE_API_TOKEN}" https://deploygate.com/api/users/${DEPLOYGATE_USER}/apps
+DG_USER=$1
+DG_TOKEN=$2
+
+if [ "$TRAVIS_BRANCH" = "develop" -a "$TRAVIS_PULL_REQUEST" = "false" ]; then
+  curl -F "file=@./Build/android.apk" -F "token=$DG_TOKEN" https://deploygate.com/api/users/$DG_USER/apps
 fi
