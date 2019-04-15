47639735f4a68122582d78fa3e3035ed0eb047c1
YdeaGames <ydeagames@gmail.com>
2019-04-15 09:12:51 +0900

    disable

--- Travis/build.sh
+++ Travis/build.sh
@@ -21,31 +21,31 @@ echo "Attempting to build $project for Windows"
  -executeMethod BuildScript.Windows \
  -quit
 
-echo "Attempting to build $project for OS X"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
- -batchmode \
- -nographics \
- -silent-crashes \
- -logFile \
- -serial ${UNITY_SERIAL_NUMBER} \
- -username ${UNITY_USER_NAME} \
- -password ${UNITY_USER_PASSWORD} \
- -projectPath $(pwd)/ \
- -executeMethod BuildScript.OSX \
- -quit
+#echo "Attempting to build $project for OS X"
+#/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+# -batchmode \
+# -nographics \
+# -silent-crashes \
+# -logFile \
+# -serial ${UNITY_SERIAL_NUMBER} \
+# -username ${UNITY_USER_NAME} \
+# -password ${UNITY_USER_PASSWORD} \
+# -projectPath $(pwd)/ \
+# -executeMethod BuildScript.OSX \
+# -quit
 
-echo "Attempting to build $project for Linux"
-/Applications/Unity/Unity.app/Contents/MacOS/Unity \
- -batchmode \
- -nographics \
- -silent-crashes \
- -logFile \
- -serial ${UNITY_SERIAL_NUMBER} \
- -username ${UNITY_USER_NAME} \
- -password ${UNITY_USER_PASSWORD} \
- -projectPath $(pwd)/ \
- -executeMethod BuildScript.Linux \
- -quit
+#echo "Attempting to build $project for Linux"
+#/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+# -batchmode \
+# -nographics \
+# -silent-crashes \
+# -logFile \
+# -serial ${UNITY_SERIAL_NUMBER} \
+# -username ${UNITY_USER_NAME} \
+# -password ${UNITY_USER_PASSWORD} \
+# -projectPath $(pwd)/ \
+# -executeMethod BuildScript.Linux \
+# -quit
 
 echo "Attempting to build $project for WebGL"
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
@@ -73,23 +73,22 @@ echo "Attempting to build $project for Android"
  -quit \
  -executeMethod BuildScript.Android
 
-# echo "Attempting to build $project for iOS"
-# /Applications/Unity/Unity.app/Contents/MacOS/Unity \
-#   -batchmode \
-#   -nographics \
-#   -silent-crashes \
-#   -logFile \
-#   -serial ${UNITY_SERIAL_NUMBER} \
-#   -username ${UNITY_USER_NAME} \
-#   -password ${UNITY_USER_PASSWORD} \
-#   -projectPath $(pwd)/ \
-#   -quit \
-#   -executeMethod BuildScript.iOS
+#echo "Attempting to build $project for iOS"
+#/Applications/Unity/Unity.app/Contents/MacOS/Unity \
+#  -batchmode \
+#  -nographics \
+#  -silent-crashes \
+#  -logFile \
+#  -serial ${UNITY_SERIAL_NUMBER} \
+#  -username ${UNITY_USER_NAME} \
+#  -password ${UNITY_USER_PASSWORD} \
+#  -projectPath $(pwd)/ \
+#  -quit \
+#  -executeMethod BuildScript.iOS
 
 echo 'Attempting to zip builds'
 cd $(pwd)/Build
-tar -czvf linux.tar.gz linux/
-hdiutil create osx.dmg -srcfolder osx/ -ov
+#tar -czvf linux.tar.gz linux/
+#hdiutil create osx.dmg -srcfolder osx/ -ov
 zip -r windows.zip windows/
-# zip -r android.zip android/
-# zip -r ios.zip ios/
+#zip -r ios.zip ios/
